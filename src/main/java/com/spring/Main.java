e7df67331a3ab00792b1a0cb677a9de3683d4ad4
abhishek <abhishekritu810219@gmail.com>
2024-04-21 09:42:53 +0530

    add address table

--- src/main/java/com/spring/Main.java
+++ src/main/java/com/spring/Main.java
@@ -4,21 +4,45 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Date;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         System.out.println("Hello world!");
 
        // making session factory object
         SessionFactory factory = new Configuration().configure().buildSessionFactory();
 
-          //making student object
+          //===================== 1/           creating student object
         Student student = new Student();
         student.setId(2);
         student.setName("anuj");
         student.setCity("Delhi");
 
-        System.out.println(student);
+
+
+        //========= creating object of Address Class =================
+        Address address = new Address();
+        address.setStreet("Street 1");
+        address.setCity("Delhi");
+        address.setOpen(true);
+        address.setX(123.45);
+        address.setAddedDate(new Date());   //import java.util.Date;
+
+        // for image reading file and converting it into byte array
+        FileInputStream fis = new FileInputStream("src/main/java/1.png");
+        byte[] data = new byte[fis.available()];
+        fis.read(data);
+        address.setImage(data);
+
+
+
+
+
+
 
 
         //making session object
@@ -29,6 +53,7 @@ public class Main {
 
         //save student object
         session.save(student);
+        session.save(address);
 
         //commit transaction
         session.getTransaction().commit();
